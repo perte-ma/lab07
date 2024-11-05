63ab0639e7d8437727292e18df32be630219e579
Marco Pertegato - marco.pertegato@studio.unibo.it <marco.pertegato@studio.unibo.it>
2024-11-05 15:59:02 +0100

    method getIterableWithPolicy implemented

--- 71-custom-iterable/src/main/java/it/unibo/inner/TestIterablePlain.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/TestIterablePlain.java
@@ -11,7 +11,7 @@ public class TestIterablePlain {
     private TestIterablePlain() {}
 
     private static <T> IterableWithPolicy<T> getIterableWithPolicy(T[] elements) {
-        return null; // TODO: return the implementation of IterableWithPolicy
+        return new ImplementIterableWithPolicy<T>(elements); // TODO: return the implementation of IterableWithPolicy
     }
 
     public static void main(final String[] args) {
